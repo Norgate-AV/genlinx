d5f07f42d0907d907d829b26f5dc244c9846f4e1
Damien Butt <damien.butt@norgateav.com>
2022-08-29 19:28:38 +0100

    refactor: make function async and create file according workspace id

--- src/commands/cfg/index.js
+++ src/commands/cfg/index.js
@@ -3,14 +3,14 @@ import { APW, CfgBuilder } from "../../../lib";
 import { getAppConfig } from "../../../lib/utils";
 
 export const cfg = {
-    create(filePath, options) {
+    async create(filePath, options) {
         try {
             const config = getAppConfig();
             const apw = new APW(filePath);
             const cfgBuilder = new CfgBuilder(apw, options, config);
             const cfg = cfgBuilder.build();
 
-            fs.writeFileSync(options.output, cfg);
+            await fs.writeFile(`${apw.id}.build.cfg`, cfg);
         } catch (error) {
             console.error(error);
             process.exit(1);
