f09d26c463743d059b71152ee8c76a5a331eca9b
Damien Butt <damien.butt@norgateav.com>
2022-08-31 16:45:00 +0100

    feat: implement archive command logic

--- src/commands/archive/index.js
+++ src/commands/archive/index.js
@@ -1,9 +1,21 @@
+import { APW, ArchiveBuilder, Options } from "../../../lib";
+import { getAppConfig } from "../../../lib/utils";
+
 export const archive = {
-    async create(filePath, options) {
+    async create(filePath, cliOptions) {
         try {
-            console.log("archive command");
-            console.log(filePath);
-            console.log(options);
+            const config = getAppConfig();
+
+            const apw = new APW(filePath);
+
+            const options = Options.getArchiveOptions(
+                apw,
+                cliOptions,
+                config.archive,
+            );
+
+            const builder = new ArchiveBuilder(apw, options);
+            builder.build();
         } catch (error) {
             console.error(error);
             process.exit(1);
