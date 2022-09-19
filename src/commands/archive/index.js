56c414e78c751b9c58e8c1aaa11ca8f241129844
Damien Butt <damien.butt@norgateav.com>
2022-09-19 19:24:57 +0100

    feat(archive-cmd): add negated boolean options

--- src/commands/archive/index.js
+++ src/commands/archive/index.js
@@ -20,20 +20,38 @@ export function archive() {
         )
         .addOption(
             new Option(
-                "-S, --include-compiled-source-files",
+                "-s, --include-compiled-source-files",
                 "include compiled source files",
             ),
         )
         .addOption(
             new Option(
-                "-M, --include-compiled-module-files",
+                "-S, --no-include-compiled-source-files",
+                "do not include compiled source files",
+            ),
+        )
+        .addOption(
+            new Option(
+                "-m, --include-compiled-module-files",
                 "include compiled module files",
             ),
         )
         .addOption(
             new Option(
-                "-N, --include-files-not-in-workspace",
+                "-M, --no-include-compiled-module-files",
+                "do not include compiled module files",
+            ),
+        )
+        .addOption(
+            new Option(
+                "-n, --include-files-not-in-workspace",
                 "include files not in workspace",
+            ),
+        )
+        .addOption(
+            new Option(
+                "-N, --no-include-files-not-in-workspace",
+                "do not include files not in workspace",
             ),
         )
         .addOption(
