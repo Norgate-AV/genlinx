f2efc1c9e9544519dfe4d48a90a57a50051d71a4
Damien Butt <damien.butt@norgateav.com>
2023-03-15 16:41:10 +0000

    refactor: split into smaller functions and resolve relative paths

--- src/actions/build/index.js
+++ src/actions/build/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import execa from "execa";
 import chalk from "chalk";
 import { NLRC } from "../../../lib";
@@ -7,6 +8,7 @@ import {
     getLocalAppConfig,
     getOptions,
     printFiles,
+    resolvePaths,
     selectFiles,
 } from "../../../lib/utils";
 
@@ -87,6 +89,84 @@ async function buildFile(file, command, options) {
     return buildResult;
 }
 
+async function executeSourceBuild(sourceFile, cliOptions, globalConfig) {
+    const localConfig = await getLocalAppConfig(path.dirname(sourceFile));
+
+    localConfig.config.build.nlrc.includePath = resolvePaths(
+        path.dirname(localConfig.path),
+        localConfig.config.build.nlrc.includePath,
+    );
+
+    localConfig.config.build.nlrc.modulePath = resolvePaths(
+        path.dirname(localConfig.path),
+        localConfig.config.build.nlrc.modulePath,
+    );
+
+    localConfig.config.build.nlrc.libraryPath = resolvePaths(
+        path.dirname(localConfig.path),
+        localConfig.config.build.nlrc.libraryPath,
+    );
+
+    const options = getOptions(
+        cliOptions,
+        localConfig.config.build,
+        globalConfig.build,
+    );
+
+    const command = NLRC.getSourceBuildCommand(sourceFile, options);
+
+    const buildResult = await buildFile(sourceFile, command, options);
+
+    const logs = getBuildLogs(buildResult);
+
+    printAllWarnings(logs.warning);
+    catchAllErrors(logs.error);
+}
+
+async function executeCfgBuild(cfgFiles, cliOptions, globalConfig) {
+    if (cfgFiles.length === 0) {
+        console.log(chalk.blue("Searching for CFG files..."));
+
+        const locatedCfgFiles = await getFilesByExtension(".cfg");
+
+        if (locatedCfgFiles.length) {
+            printFiles(locatedCfgFiles);
+            cfgFiles.push(...locatedCfgFiles);
+        }
+    }
+
+    if (cfgFiles.length === 0) {
+        console.log(chalk.red("No CFG files found."));
+        process.exit();
+    }
+
+    if (shouldPromptUser(cliOptions, cfgFiles)) {
+        const selectedCfgFiles = await selectFiles(cfgFiles);
+
+        cfgFiles.splice(0, cfgFiles.length);
+        cfgFiles.push(...selectedCfgFiles);
+    }
+
+    for (const cfgFile of cfgFiles) {
+        const localConfig = await getLocalAppConfig(cfgFile);
+
+        const { build: options } = getOptions(
+            { build: cliOptions },
+            localConfig.store,
+            globalConfig.store,
+        );
+
+        const command = NLRC.getCfgBuildCommand(cfgFile, options);
+
+        const buildResult = await buildFile(cfgFile, command, options);
+
+        const logs = getBuildLogs(buildResult);
+
+        printAllWarnings(logs.warning);
+        catchAllErrors(logs.error);
+    }
+}
+
 export const build = {
     async execute(cliOptions) {
         try {
@@ -95,70 +175,11 @@ export const build = {
             const globalConfig = await getGlobalAppConfig();
 
             if (sourceFile) {
-                const localConfig = await getLocalAppConfig(sourceFile);
-                const options = getOptions(
-                    cliOptions,
-                    localConfig.build,
-                    globalConfig.build,
-                );
-
-                const command = NLRC.getSourceBuildCommand(sourceFile, options);
-
-                const buildResult = await buildFile(
-                    sourceFile,
-                    command,
-                    options,
-                );
-
-                const logs = getBuildLogs(buildResult);
-
-                printAllWarnings(logs.warning);
-                catchAllErrors(logs.error);
-
+                await executeSourceBuild(sourceFile, cliOptions, globalConfig);
                 process.exit();
             }
 
-            if (cfgFiles.length === 0) {
-                console.log(chalk.blue("Searching for CFG files..."));
-
-                const locatedCfgFiles = await getFilesByExtension(".cfg");
-
-                if (locatedCfgFiles.length) {
-                    printFiles(locatedCfgFiles);
-                    cfgFiles.push(...locatedCfgFiles);
-                }
-            }
-
-            if (cfgFiles.length === 0) {
-                console.log(chalk.red("No CFG files found."));
-                process.exit();
-            }
-
-            if (shouldPromptUser(cliOptions, cfgFiles)) {
-                const selectedCfgFiles = await selectFiles(cfgFiles);
-
-                cfgFiles.splice(0, cfgFiles.length);
-                cfgFiles.push(...selectedCfgFiles);
-            }
-
-            for (const cfgFile of cfgFiles) {
-                const localConfig = await getLocalAppConfig(cfgFile);
-
-                const { build: options } = getOptions(
-                    { build: cliOptions },
-                    localConfig.store,
-                    globalConfig.store,
-                );
-
-                const command = NLRC.getCfgBuildCommand(cfgFile, options);
-
-                const buildResult = await buildFile(cfgFile, command, options);
-
-                const logs = getBuildLogs(buildResult);
-
-                printAllWarnings(logs.warning);
-                catchAllErrors(logs.error);
-            }
+            await executeCfgBuild(cfgFiles, cliOptions, globalConfig);
         } catch (error) {
             console.error(error);
             process.exit(1);
