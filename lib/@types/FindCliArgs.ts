61770f2a0e43b51ac34ceb53403f382e120040e2
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-08-02 14:23:12 +0100

    feat: integrate findlinx with the find command

---
+++ lib/@types/FindCliArgs.ts
@@ -0,0 +1,3 @@
+export type FindCliArgs = {
+    json?: boolean;
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,4 @@ export * from "./archive.js";
 export * from "./build.js";
 export * from "./cfg.js";
 export * from "./config.js";
+export * from "./find.js";
