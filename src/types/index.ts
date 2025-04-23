47c9a0ca2b5146a82273d21c53474b5c6ed332d4
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-04-23 20:27:27 +0100

    refactor: rename type files

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,6 @@ export * from "./archive.js";
 export * from "./build.js";
 export * from "./cfg.js";
 export * from "./cli.js";
-export * from "./config.js";
-export * from "./file.js";
+export * from "./config1.js";
+export * from "./file1.js";
 export * from "./utils.js";
